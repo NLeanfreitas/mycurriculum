f8a00781cae245698674cea1a2cca21a74ec2cab
leandro.rosa@alunos.sc.senac.br <leandro.rosa@alunos.sc.senac.br>
2021-06-24 16:54:39 -0300

    atualizei algumas coisas

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ var habilidade = function () {
         div.classList.remove('Saas');
     } else {
         div.classList.add('Saas');
-        div.innerHTML = '<h2>HABILIDADES</h2><br> <p>Explicando minhas habilidades</p>';
+        div.innerHTML = '<h2>HABILIDADES</h2><br> <p>Só sei jogar xadrez :c</p> <img src="assets/images/photo1.jpg">';
     }
 }
 var sobre = function () {
@@ -16,6 +16,16 @@ var sobre = function () {
     } else {
         div.classList.add('Saas');
         div.innerHTML = '<h2>SOBRE MIM</h2><br> <p>Falando sobre mim</p>';
+    }
+}
+var manga = function () {
+    var div = document.getElementById('manga');
+    if (div.classList.contains('Saas')) {
+        div.classList.remove('Saas');
+        div.innerHTML="";
+    } else {
+        div.classList.add('Saas');
+        div.innerHTML = '<h2>MANGAS</h2><br> <p>Mangas que já li</p> <img src="assets/images/manga1.jpg"> <img src="assets/images/manga2.jpg">';
     }
 }
 
